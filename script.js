5c06600e499b9b52d1d5cd7703d550e8385a3ec4
KsuDeihina <deihina1996@mail.ru>
2022-09-12 15:08:27 +0500

    JSburger

--- script.js
+++ script.js
@@ -30,11 +30,12 @@ function time100(){
 
 const addCartBtn =  document.querySelector('.addCart'),
 BtnCart = document.querySelector('.receipt__window-btn'),
-// productBtns = document.querySelectorAll('.main__product-btn'),
-// productAmount = document.querySelectorAll('.main__product-num'),
+productBtns = document.querySelectorAll('.main__product-btn'),
+productAmount = document.querySelectorAll('.main__product-num'),
 checkList = document.querySelector('.receipt'),
 check = document.querySelector('.receipt__window-out'),
 overBasket = document.querySelector('.receipt__window-totalPrice')
+
 
 
 
@@ -45,8 +46,12 @@ const product = {
       amount: 0,
       img: 'images/product2.jpg',
       call: 280,
+      add: 0,
       get totalCall(){
         return this.call * this.amount
+      },
+      get totalAdd(){
+        return this.add * this.amount
       },
       get totalSum(){
         return this.price * this.amount
@@ -58,8 +63,12 @@ const product = {
       amount: 0,
       img: 'images/product1.jpg',
       call: 280,
+      add: 0,
       get totalCall(){
         return this.call * this.amount
+      },
+      get totalAdd(){
+        return this.add * this.amount
       },
       get totalSum(){
         return this.price * this.amount
@@ -71,8 +80,12 @@ const product = {
       amount: 0,
       img: 'images/product3.jpg',
       call: 280,
+      add: 0,
       get totalCall(){
         return this.call * this.amount
+      },
+      get totalAdd(){
+        return this.add * this.amount
       },
       get totalSum(){
         return this.price * this.amount
@@ -88,8 +101,6 @@ addCartBtn.addEventListener('click', function () {
 BtnCart.addEventListener('click', function () {
     checkList.classList.remove('active');
     location.reload()
-    // checkList.style = `display: none;
-    // opacity: 0;`
     })
 
     
@@ -99,12 +110,33 @@ window.addEventListener('click', (e) => {
         const parent = e.target.closest('.main__product')           
         if (parent){
              const idParent = parent.getAttribute('id')
-             if(attr == '-') product[idParent].amount--
+             if(attr == '-' && product[idParent].amount > 0) product[idParent].amount--
              else if(attr == '+') product[idParent].amount++
             basket()
         }           
     }       
 })
+
+
+
+
+
+window.addEventListener('click', (e) => {       
+    if(e.target.classList.contains('main__product-checkbox')){
+        const attr = e.target.getAttribute('data-extra')
+        const parent = e.target.closest('.main__product')           
+        if (parent){
+                const idParent = parent.getAttribute('id')
+                if(attr == 'doubleMayonnaise') product[idParent].add = product[idParent].add + 1000 
+                else if(attr == 'lettuce') product[idParent].add = product[idParent].add + 2000
+                else if(attr == 'cheese') product[idParent].add = product[idParent].add + 3000 
+                basket()
+        }           
+    }         
+})
+
+
+
 
 
 function basket() {
@@ -113,17 +145,21 @@ function basket() {
         const productObj = product[key]
         const productCard = document.querySelector(`#${productObj.name}`),
         countIndecator = productCard.querySelector('.main__product-num'),
-        countPrice = productCard.querySelector('.main__product-price'),
-        countCall = productCard.querySelector('.main__product-call')
-         if(productObj.amount && productObj.amount >= 0){
-             productArray.push(productObj)
+        countPrice = productCard.querySelector('.main__product-price span'),
+        countCall = productCard.querySelector('.main__product-call span')       
+         if(productObj.amount && productObj.amount > 0){
+            productArray.push(productObj)
             countIndecator.innerHTML = productObj.amount
-            countPrice.innerHTML = productObj.totalSum
+            countPrice.innerHTML = productObj.totalSum + productObj.totalAdd
             countCall.innerHTML = productObj.totalCall
+            
         } else {
-            countIndecator.innerHTML = 0                           
+            countIndecator.innerHTML = 0
+            countPrice.innerHTML = 0 
+            countCall.innerHTML = 0                          
         }
-        
+        console.log(productObj.totalAdd);
+
      }
      check.innerHTML = ''
 
